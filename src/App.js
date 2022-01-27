1a2ede189a59e59eb2b307ece4b2bf05565bbef6
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-01-27 11:43:48 +0100

    Convert App Component to use classes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { 
+    todos: [
+      {id: 1, content: 'buy some milk'},
+      {id: 2, content: 'play mario kart'}
+    ]
+   } 
+  render() { 
+    return ();
+  }
 }
-
+ 
 export default App;
