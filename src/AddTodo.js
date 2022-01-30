9eb4b48034d816480a80a0e0e578cc7d1423af00
Temitope Ogunleye <topeogunleye1@gmail.com>
2022-01-30 12:59:31 +0100

    Add add todo, delete todo functionality and styles

---
+++ src/AddTodo.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class AddTodo extends Component {
+  state = { 
+    content: ''
+  } 
+
+  handleChange = (e) => {
+    this.setState({
+      content: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.addTodo(this.state)
+  }
+  render() { 
+    return (
+      <div>
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="">Add new todo</label>
+        <input type="text" onChange={this.handleChange}/>
+      </form>
+      </div>
+    );
+  }
+}
+ 
+export default AddTodo;
